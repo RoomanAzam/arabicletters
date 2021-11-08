8268ff99bb78d692b7e1fd81d740c38dd83df1df
RoomanAzam <bsef18a532@pucit.edu.pk>
2021-11-08 09:56:15 -0800

    firstpage ready

---
+++ app/src/main/java/com/example/arabicletters/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.arabicletters;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.textView7);
+        button.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+
+
+
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, learn_letters.class);
+        startActivity(intent);
+    }
+
+}
